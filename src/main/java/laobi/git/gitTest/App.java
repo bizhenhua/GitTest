462b2a66285f1dc562ed85654ad112dacfb1ea01
bizhenhua <1139443988@qq.com>
2016-01-21 16:12:33 +0800

    add tom

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -6,11 +6,6 @@ package laobi.git.gitTest;
  */
 public class App {
 	public static void main(String[] args) {
-		String jack = "jack";
-		System.out.println(jack);
-		String name = "xxxx";
-		System.out.println(name);
-		System.out.println("aaaa");
-		System.out.println("cccc");
+		String tom = "tom";
 	}
 }
