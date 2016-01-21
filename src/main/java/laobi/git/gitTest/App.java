8c5b1648ec1b420a6e21dd9f86e122a5868e60f7
bizhenhua <1139443988@qq.com>
2016-01-21 09:54:15 +0800

    add age

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -8,5 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		String ddd = "jack";
 		System.out.println("ddd = " + ddd);
+		int age = 18;
+		System.out.println("age = " + age);
 	}
 }
