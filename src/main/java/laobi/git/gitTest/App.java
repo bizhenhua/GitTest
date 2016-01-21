1b898ed91c21c898c7edba88441b790767a36a76
bizhenhua <1139443988@qq.com>
2016-01-21 16:44:47 +0800

    update name

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -6,11 +6,7 @@ package laobi.git.gitTest;
  */
 public class App {
 	public static void main(String[] args) {
-		String ddd = "jack";
-		System.out.println("ddd = " + ddd);
-		int age = 18;
-		System.out.println("age = " + age);
-		String father = "bzh";
-		System.out.println("father = " + father);
+		String name = "jack";
+		System.out.println("name = " + name);
 	}
 }
