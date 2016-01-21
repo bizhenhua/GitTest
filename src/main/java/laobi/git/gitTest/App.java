de2d4fcc4d40e000f5756503d528ae809f54fdfa
bizhenhua <1139443988@qq.com>
2016-01-21 16:58:03 +0800

    10-20

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -8,6 +8,8 @@ public class App {
 	public static void main(String[] args) {
 		String name = "tom";
 		System.out.println("name = " + name);
+		String age = "20";
+		System.out.println("age = " + age);
 		System.out.println("aaaaaaaaaaaaaa");
 		String age = "18";
 		System.out.println("age = " + age);
