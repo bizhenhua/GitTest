739ff9ec69305d6baee90b3ff5d379d95a2c8924
bizhenhua <1139443988@qq.com>
2016-01-21 15:06:24 +0800

    xxx

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -7,6 +7,10 @@ package laobi.git.gitTest;
 public class App {
 	public static void main(String[] args) {
 		String jack = "jack";
+<<<<<<< Upstream, based on ORIG_HEAD
 		System.out.println(jack);
+=======
+		String name = "xxxx";
+>>>>>>> 256ef01 xxx
 	}
 }
