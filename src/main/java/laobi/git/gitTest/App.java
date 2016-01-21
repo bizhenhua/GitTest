ee26b2873e7f5facfb6b20975b91ced187579e6f
bizhenhua <1139443988@qq.com>
2016-01-21 13:43:22 +0800

    add grandfather

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -10,7 +10,7 @@ public class App {
 		System.out.println("ddd = " + ddd);
 		int age = 18;
 		System.out.println("age = " + age);
-		String father = "bzh";
-		System.out.println("father = " + father); 
+		String grandfather = "bzh";
+		System.out.println("grandfather = " + grandfather); 
 	}
 }
