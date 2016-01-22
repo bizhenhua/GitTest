758e4af6eb9c2bd285d6d0b3833f9fdf6bc6202d
bizhenhua <1139443988@qq.com>
2016-01-22 15:46:05 +0800

    fixed

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -8,5 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("m1");
 		System.out.println("d1");
+		System.out.println("m2");
+		System.out.println("m3");
 	}
 }
