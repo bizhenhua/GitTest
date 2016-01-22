7eeefece689802b7a7a5858459268840986cf7bb
bizhenhua <1139443988@qq.com>
2016-01-22 11:26:36 +0800

    String la = "la";

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -6,8 +6,6 @@ package laobi.git.gitTest;
  */
 public class App {
 	public static void main(String[] args) {
-		String name = "tom";
-		String aaa = "tom";
-		String bbb = "tom";
+		String la = "la";
 	}
 }
