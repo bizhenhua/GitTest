4e8bbb2409932670c2301be49750f2131e504e09
bizhenhua <1139443988@qq.com>
2016-01-22 13:27:24 +0800

    String lb = "lb";

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -8,6 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		String ya = "ya";
 		String la = "la";
+		String lb = "lb";
 		String name = "tom";
 		System.out.println("name = " + name);
 	}
