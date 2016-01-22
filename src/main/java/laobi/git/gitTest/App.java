2f7cf59d0b9730ef9794088efa4d08ae40e23fe0
bizhenhua <1139443988@qq.com>
2016-01-22 17:02:24 +0800

    init System.out.println("m1");

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -6,10 +6,6 @@ package laobi.git.gitTest;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("master1");
-		System.out.println("dev1");
-		System.out.println("dev2");
-		System.out.println("master2");
-		System.out.println("master3");
+		System.out.println("m1");
 	}
 }
