71d56584b32e41c9164e50d2fbc3ad595cd0a502
bizhenhua <1139443988@qq.com>
2016-01-22 11:14:34 +0800

    String name = "tom";

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -7,17 +7,5 @@ package laobi.git.gitTest;
 public class App {
 	public static void main(String[] args) {
 		String name = "tom";
-		String age = "20";
-		String father = "bzh";
-		String mother = "jenny";
-		String gmother = "wrm";
-		String gfather = "bgs";
-		System.out.println();
-		String aaa = "bgs";
-		String ccc = "bgs";
-		String ddd = "bgs";
-		String fff = "bgs";
-		String ggg = "bgs";
-		String hhh = "bgs";
 	}
 }
