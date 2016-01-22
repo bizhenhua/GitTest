7929197662660d2947f39b05dcb571ec242596df
bizhenhua <1139443988@qq.com>
2016-01-22 14:01:15 +0800

    String name = "jack";

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -6,11 +6,6 @@ package laobi.git.gitTest;
  */
 public class App {
 	public static void main(String[] args) {
-		String name = "tom";
-		System.out.println("name = " + name);
-		System.out.println("aaaaaaaaaaaaaa");
-		System.out.println("bbbbbbbbbbbb");
-		System.out.println("d");
-		System.out.println("f");
+		String name = "jack";
 	}
 }
