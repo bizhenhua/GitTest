17b520b6581976ef5348176f4ad7f0d39fe822ba
bizhenhua <1139443988@qq.com>
2016-01-22 10:14:05 +0800

    tom

--- src/main/java/laobi/git/gitTest/App.java
+++ src/main/java/laobi/git/gitTest/App.java
@@ -7,7 +7,6 @@ package laobi.git.gitTest;
 public class App {
 	public static void main(String[] args) {
 		String name = "tom";
-		System.out.println("name = " + name);
 		String age = "20";
 		System.out.println("age = " + age);
 		System.out.println("aaaaaaaaaaaaaa");
